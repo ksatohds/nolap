235b5832dde4c8ffb7ce102279110840c12d7049
ksatohds <kenichi-satoh@biwako.shiga-u.ac.jp>
2025-02-05 17:13:11 +0900

    20250205 1713

--- R/nolap.R
+++ R/nolap.R
@@ -27,13 +27,18 @@
 #' @examples
 #' x <- cars$speed
 #' y <- cars$dist
-#' res <- nolap(x=x,y=y,nseed=20)
-#' plot(res$x,res$y,col=2)
-# check movements
-#' plot(x,y)
+#' res <- nolap(x=x,y=y,xdiv=10,ydiv=10,nseed=10)
+#' print(res$seed)
+#' plot(res$x,res$y,col=2,xlim=range(res$xlims),ylim=range(res$ylims))
+#' abline(h=res$ylims,col=8)
+#' abline(v=res$xlims,col=8)
+#' # check movements
+#' plot(x,y,xlim=range(res$xlims),ylim=range(res$ylims),type="n")
+#' text(x,y,1:50)
 #' abline(h=res$ylims,col=8)
 #' abline(v=res$xlims,col=8)
 #' arrows(x,y,res$x,res$y,length=0.1,col=ifelse(res$is.moved,2,4))
+#' legend("topleft",legend=c("moved","stay"),fill=c(2,4))
 
 nolap <- function(x,y,xdiv=100,ydiv=70,xlim=range(x),ylim=range(y),seed=NULL,nseed=10){
   makedxy <- function(x,y,xdiv,ydiv,xlim,ylim){
